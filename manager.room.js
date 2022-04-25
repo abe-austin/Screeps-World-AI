e63b4383f6157b94e4d62aeda73bd5e3a8e26e37
Abe Austin <aaustin@efilecabinet.com>
2022-04-25 16:59:34 -0600

    Update to room AI

--- manager.room.js
+++ manager.room.js
@@ -1,18 +1,4 @@
-var owned_rooms = 
-    [
-        {
-            "name":"W7N2",
-            "resources":
-            [
-                {
-                    "x":25,
-                    "y":14,
-                    "type":"ENERGY",
-                    "accessible_points":4
-                }
-            ]
-        }
-    ];
+var owned_rooms = [];
 
 var RoomManager = 
 {
@@ -34,23 +20,31 @@ var RoomManager =
         //THAT GETS PASSED ON TO THE SPAWNER INSIDE THE ROOM, SO IT CAN DETERMINE WHEN AND WHAT TO GENERATE
     },
     
-    ParseRoom: function(name)
+    ParseRoom: function(roomName)
     {
-        var room = Game.rooms[name];
-        var roomStructure = {};
-        var sources = room.find(FIND_SOURCES);
+        var room = Game.rooms[roomName];
+        var roomStructure = { "name": roomName } ;
+        roomStructure.resources = room.find(this.ParseResource(room, FIND_SOURCES));
+        roomStructure.minerals = room.find(this.ParseResource(room, FIND_MINERALS));
+        owned_rooms.push(roomStructure);
+    },
+    
+    ParseResource: function(room, resourceType)
+    {
         var that = this;
-        room.find(FIND_SOURCES)
+        var resourceList = [];
+        room.find(resourceType)
             .forEach(function(source)
             {
                 var accessiblePositionsNum = that.NumberOfAccessiblePositions(room, source.pos);
-                console.log(source.pos.x + "," + source.pos.y + ":" + accessiblePositionsNum);
-                //SET roomStructure AND ADD TO LIST OF ROOMS
-                //ALSO RETRIEVE MINERALS ALONG WITH SOURCES
-                //SEEMS LIKE MINERALS WILL COME INTO PLAY LATER, WHEN I HAVE A FEW TO COMBINE
-                //SO FOR NOW PUT THEM IN THEIR OWN CATEGORY, ALONG WITH NUM OF ACCESSIBLE POSITIONS
-                //BUT DON'T HAVE THEM AS PART OF THE QUOTA UNTIL LATER
+                resourceList.push({
+                    "x": source.pos.x, 
+                    "y": source.pos.y, 
+                    "accessible_points": accessiblePositionsNum,
+                    "dedicated_units": []
+                });
             });
+        return resourceList;
     },
     
     NumberOfAccessiblePositions: function(room, pos)
--- manager.spawner.js
+++ manager.spawner.js
@@ -2,6 +2,9 @@ var SpawnerManager =
 {
     Process: function() 
     {
+        //Get all of your spawners by room. Load from manager.room your resources
+        //Update job pool
+        //Execute next item on pool
     }
 };
 
